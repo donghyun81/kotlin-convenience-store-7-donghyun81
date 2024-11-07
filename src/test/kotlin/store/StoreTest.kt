db98a05f4b926a1759ce8efc5384dfdeda714a1d
윤동현 <82762769+donghyun81@users.noreply.github.com>
2024-11-07 22:07:19 +0900

    test(StoreTest): 코드의 줄 수를 줄이기 위해서 수정
    - store를 setUp에서 초기화해서 사용
    - promosition과 products 필요 없는 값 제거

--- src/test/kotlin/store/StoreTest.kt
+++ src/test/kotlin/store/StoreTest.kt
@@ -6,20 +6,25 @@ import org.junit.jupiter.api.Test
 
 class StoreTest {
     private lateinit var defaultProducts: List<Product>
+    private lateinit var defaultPromotion: List<Promotion>
+    private lateinit var store: Store
 
     @BeforeEach
-    fun `setUp`() {
+    fun setUp() {
         defaultProducts = listOf(
             Product("사과", 1000, 10, "MD추천 상품"),
             Product("사과", 1000, 10, "null"),
-            Product("수박", 1000, 10, "MD추천 상품"),
         )
+        defaultPromotion = listOf(
+            Promotion("MD추천상품", 1, 1, "2024-01-01", "2024-12-31"),
+            Promotion("반짝할인", 1, 1, "2024-11-01", "2024-11-30")
+        )
+        store = Store(defaultProducts, defaultPromotion)
     }
 
     @Test
     fun `각 상품의 재고 수량을 고려하여 결제 가능 여부 성공 테스트`() {
-        val store = Store(defaultProducts)
-        val purchaseProduct = Product("사과", 1000, 14, "MD추천 상품")
+        val purchaseProduct = RequestedProduct("사과", 14)
         val result = store.hasProduct(purchaseProduct)
         val expected = true
         assertEquals(expected, result)
@@ -27,28 +32,19 @@ class StoreTest {
 
     @Test
     fun `각 상품의 재고 수량을 고려하여 결제 가능 여부 싪패 테스트`() {
-        val products = mutableListOf(Product("사과", 1000, 10, "MD추천 상품"))
-        val store = Store(products)
-        val purchaseProduct = Product("사과", 1000, 11, "MD추천 상품")
+        val purchaseProduct = RequestedProduct("사과", 21)
         val result = store.hasProduct(purchaseProduct)
         val expected = false
         assertEquals(expected, result)
     }
 
-
     @Test
     fun `고객이 상품을 구매할 때마다, 결제된 수량만큼 해당 상품의 재고에서 차감하고 최신 상태를 반환하는 테스트`() {
-        val store = Store(defaultProducts)
-        val purchaseProduct = Product("사과", 1000, 14, "MD추천 상품")
-
-        store.buyProduct(purchaseProduct)
-
+        store.buyProduct(RequestedProduct("사과", 14))
         val result = listOf(
             Product("사과", 1000, 0, "MD추천 상품"),
-            Product("사과", 1000, 6, "null"),
-            Product("수박", 1000, 10, "MD추천 상품")
+            Product("사과", 1000, 6, "null")
         )
-
         val expected = store.getProducts()
         assertEquals(expected, result)
     }
