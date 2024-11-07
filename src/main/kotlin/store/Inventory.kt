ef242bc634df8da361a9b3af2fb663a084acb940
윤동현 <82762769+donghyun81@users.noreply.github.com>
2024-11-07 12:26:22 +0900

    feat(Inventory): 제품을 구매하는 기능 추가
    - Inventory가 가지고 있는 제고에서 구매한 수량을 빼는 기능 추가

--- src/main/kotlin/store/Inventory.kt
+++ src/main/kotlin/store/Inventory.kt
@@ -15,4 +15,25 @@ class Inventory(
         }
         return currentPurchaseCount < 0
     }
+
+    fun buyProduct(purchaseProduct: Product) {
+        updateProductsQuantity(purchaseProduct)
+    }
+
+    private fun updateProductsQuantity(purchaseProduct: Product): Int {
+        var purchaseRemainingQuantity = purchaseProduct.getQuantity()
+        products.replaceAll { product ->
+            if (product.name == purchaseProduct.name && purchaseRemainingQuantity > 0) {
+                purchaseRemainingQuantity = applyPurchaseToProduct(product, purchaseRemainingQuantity)
+            }
+            product
+        }
+        return purchaseRemainingQuantity
+    }
+
+    private fun applyPurchaseToProduct(product: Product, purchaseRemainingQuantity: Int): Int {
+        val excess = product.calculateExcessQuantity(purchaseRemainingQuantity)
+        product.deductQuantity(purchaseRemainingQuantity)
+        return excess
+    }
 }
