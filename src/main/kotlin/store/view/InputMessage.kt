a51c6d046af79bb0db25a77931b6ad267a8e3395
윤동현 <82762769+donghyun81@users.noreply.github.com>
2024-11-10 20:35:06 +0900

    refactor(InputMessage): 출력할 메세지를 상수로 보관

---
+++ src/main/kotlin/store/view/InputMessage.kt
@@ -0,0 +1,7 @@
+package store.view
+
+enum class InputMessage(val message: String) {
+    PURCHASE_PRODUCTS("구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])"),
+    MEMBERSHIP_DISCOUNT("멤버십 할인을 받으시겠습니까? (Y/N)"),
+    ADDITIONAL_PURCHASE("감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)")
+}
